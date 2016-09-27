be53677630088b41ee3d9baad1545a645a5cc34f
Artur Vasilov <artur.vasilov@dz.ru>
2016-09-27 22:45:45 +0300

    updated samples for lesson 8

--- PictureOfDayMVVM/app/src/main/java/ru/gdgkazan/pictureofdaymvvm/repository/DefaultNasaRepository.java
+++ PictureOfDayMVVM/app/src/main/java/ru/gdgkazan/pictureofdaymvvm/repository/DefaultNasaRepository.java
@@ -2,7 +2,7 @@ package ru.gdgkazan.pictureofdaymvvm.repository;
 
 import android.support.annotation.NonNull;
 
-import ru.arturvasilov.rxloader.RxSchedulers;
+import ru.arturvasilov.rxloader.RxUtils;
 import ru.gdgkazan.pictureofdaymvvm.api.ApiFactory;
 import ru.gdgkazan.pictureofdaymvvm.content.DayPicture;
 import rx.Observable;
@@ -17,6 +17,6 @@ public class DefaultNasaRepository implements NasaRepository {
     public Observable<DayPicture> dayPicture() {
         return ApiFactory.getNasaService()
                 .todayPicture()
-                .compose(RxSchedulers.async());
+                .compose(RxUtils.async());
     }
 }
